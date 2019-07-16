f966fc4878d1bad6f4a52cb6c874263f42a0bfda
AnhVietPham <pham.anh.viet.vlth@gmail.com>
2019-07-16 13:08:13 +0700

    Add class Node for DoublyLinkedList.java

--- src/com/avp/basic/datastructure_algorithms/DoublyLinkedList.java
+++ src/com/avp/basic/datastructure_algorithms/DoublyLinkedList.java
@@ -1,4 +1,39 @@
 package com.avp.basic.datastructure_algorithms;
 
 public class DoublyLinkedList {
+    private static class Node<E>{
+        private E element;
+        private Node<E> next;
+        private Node<E> prev;
+
+        Node(E e, Node<E> next, Node<E> prev){
+            this.element = e;
+            this.next = next;
+            this.prev = prev;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public void setElement(E element) {
+            this.element = element;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
+
+        public Node<E> getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node<E> prev) {
+            this.prev = prev;
+        }
+    }
 }
