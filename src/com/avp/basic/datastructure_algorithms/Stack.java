9fff61f5eac1eba3172db9f54428e33118a1f044
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-08-01 06:27:50 +0700

    Interface Stack with comment in Javadoc Style.

--- src/com/avp/basic/datastructure_algorithms/Stack.java
+++
@@ -1,4 +0,0 @@
-package com.avp.basic.datastructure_algorithms;
-
-public class Stack {
-}
---
+++ src/com/avp/basic/datastructure_algorithms/stacks/Stack.java
@@ -0,0 +1,46 @@
+package com.avp.basic.datastructure_algorithms.stacks;
+
+
+/**
+ * This is a Stack class. It differs from java.util.Stack. This class supports
+ * inserted and removed according to First-Out Last-Out principle.
+ *
+ * @author AnhVietPham
+ */
+public interface Stack<E> {
+
+    /**
+     * Returns the number of element in the Stack
+     *
+     * @return number of element in the Stack
+     */
+    int size();
+
+    /**
+     * Check whether the Stack is empty
+     *
+     * @return true if the Stack is empty
+     */
+    boolean isEmpty();
+
+    /**
+     * Insert a element at the top of the Stack
+     *
+     * @param e element to be inserted
+     */
+    void push(E e);
+
+    /**
+     * Return element at the top of the Stack
+     *
+     * @return top element in the Stack (or null if empty)
+     */
+    E top();
+
+    /**
+     * Remove and return the top element which is removed from the Stack
+     *
+     * @return element removed (or null if empty)
+     */
+    E pop();
+}
