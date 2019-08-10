22018e42cbceab0ce439940a2b2fc46565bbb943
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-08-10 07:22:22 +0700

    Homework Tree: Add BinaryTree.java

---
+++ src/com/avp/basic/datastructure_algorithms/trees/BinaryTree.java
@@ -0,0 +1,10 @@
+package com.avp.basic.datastructure_algorithms.trees;
+
+
+public interface BinaryTree<E> extends Tree<E> {
+    Position<E> left(Position<E> p);
+
+    Position<E> right(Position<E> p);
+
+    Position<E> sibling(Position<E> p);
+}
