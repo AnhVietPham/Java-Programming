7c5b0b1489712f7d5436742564fb78c8312b74be
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-08-10 07:16:34 +0700

    Homework Tree: Add Tree.java

---
+++ src/com/avp/basic/datastructure_algorithms/trees/Tree.java
@@ -0,0 +1,27 @@
+package com.avp.basic.datastructure_algorithms.trees;
+
+import java.util.Iterator;
+
+public interface Tree<E> extends Iterable<E> {
+    Position<E> root();
+
+    Position<E> parent(Position<E> p);
+
+    Iterable<Position<E>> children(Position<E> p);
+
+    int numChildren(Position<E> p);
+
+    boolean isInternal(Position<E> p);
+
+    boolean isExternal(Position<E> p);
+
+    boolean isRoot(Position<E> p);
+
+    int size();
+
+    boolean isEmpty();
+
+    Iterator<E> iterator();
+
+    Iterable<Position<E>> postions();
+}
